7756deeb9cf08bf5567e1540ca3ed10441cccdce
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2023-11-28 21:42:44 +0100

    Organize code, rework ship preview

---
+++ app/play/components/Prepare/index.js
@@ -0,0 +1,147 @@
+import Board from "@/components/Board";
+import generateRandomColor from "@/lib/generateRandomColor";
+import generateRandomShips from "@/lib/generateRandomShips";
+import generateShipCells from "@/lib/generateShipCells";
+import generateShipObject from "@/lib/generateShipObject";
+import isShipInvalid from "@/lib/isShipInvalid";
+import { useEffect, useState } from "react";
+
+export default function Prepare({ ships, setShips, startGame }) {
+    const [currentShip, setCurrentShip] = useState(null); // current ship being placed
+
+    useEffect(() => {
+        if (!currentShip) return;
+
+        function directionOnScroll(e) {
+            const directions = ["right", "down", "left", "up"];
+
+            let current_direction_index = directions.indexOf(currentShip.direction);
+
+            if (e.deltaY > 0) {
+                // if scrolling down, change direction clockwise
+                if (current_direction_index === 3) current_direction_index = 0;
+                else current_direction_index++;
+            } else {
+                // if scrolling up, change direction counter-clockwise
+                if (current_direction_index === 0) current_direction_index = 3;
+                else current_direction_index--;
+            }
+
+            setCurrentShipDirection(directions[current_direction_index]);
+        }
+
+        document.addEventListener("wheel", directionOnScroll);
+
+        return () => document.removeEventListener("wheel", directionOnScroll);
+    }, [currentShip]);
+
+    useEffect(() => {
+        if (!currentShip) return;
+
+        if (ships.length >= 5) setCurrentShip(null); // if all ships are placed, make sure currentShip is null
+        else {
+            setCurrentShipColor(generateRandomColor()); // generate new color for next ship
+            setCurrentShipCells(); // reset current ship cells
+        }
+    }, [ships.length]); // on ships.length change, update current ship
+
+    function setCurrentShipDirection(direction) {
+        if (!currentShip) return;
+
+        setCurrentShip((prev) => {
+            const new_ship = {
+                ...prev,
+                direction,
+                cells: generateShipCells(5 - ships.length, prev.cells[0].row, prev.cells[0].column, direction),
+            };
+
+            const invalid = isShipInvalid(new_ship, ships);
+
+            if (invalid) new_ship.invalid = true;
+            else delete new_ship.invalid;
+
+            return new_ship;
+        });
+    }
+
+    function setCurrentShipCells(
+        length = 5 - ships.length,
+        row = currentShip?.cells[0]?.row,
+        column = currentShip?.cells[0]?.column
+    ) {
+        if (!currentShip) return;
+
+        setCurrentShip((prev) => {
+            const new_ship = {
+                ...prev,
+                cells: generateShipCells(length, row, column, prev.direction),
+            };
+
+            const invalid = isShipInvalid(new_ship, ships);
+
+            if (invalid) new_ship.invalid = true;
+            else delete new_ship.invalid;
+
+            return new_ship;
+        });
+    }
+
+    function setCurrentShipColor(color) {
+        if (!currentShip) return;
+
+        setCurrentShip({
+            ...currentShip,
+            color,
+        });
+    }
+
+    function generateCurrentShip(row, column, length = 5 - ships.length) {
+        if (length < 1) return;
+
+        const new_ship = generateShipObject("right", length, row, column);
+
+        const invalid = isShipInvalid(new_ship, ships);
+
+        if (invalid) new_ship.invalid = true;
+
+        setCurrentShip(new_ship);
+    }
+
+    function handleCellHover(row, column) {
+        if (ships.length >= 5) return; // if all ships are placed, don't do anything
+
+        if (currentShip) setCurrentShipCells(5 - ships.length, row, column);
+        else generateCurrentShip(row, column);
+    }
+
+    function addCurrentShip() {
+        if (!currentShip || currentShip.invalid) return;
+
+        setShips((ships) => [...ships, currentShip]);
+    }
+
+    return (
+        <div>
+            <h1>place yo ships</h1>
+            <button onClick={() => setShips(generateRandomShips())}>random placement</button>
+            <button onClick={() => setShips([])}>reset board</button>
+            <Board
+                onCellHover={handleCellHover}
+                currentShip={currentShip}
+                onCellClick={addCurrentShip}
+                ships={ships}
+                showOccupied={true}
+            />
+            {ships.length >= 5 ? (
+                <div>
+                    you placed all ships <button onClick={startGame}>Start</button>
+                </div>
+            ) : (
+                <>
+                    {currentShip && <div>current direction: {currentShip.direction}</div>}
+                    <div>you have {5 - ships.length} ships left to place</div>
+                </>
+            )}
+        </div>
+    );
+}
--- app/play/page.js
+++ app/play/page.js
@@ -1,93 +1,13 @@
 "use client";
 
-import Board from "@/components/Board";
-import generateShipObject from "@/lib/generateShipObject";
-import isShipInvalid from "@/lib/isShipInvalid";
-import { useEffect, useState } from "react";
-import generateRandomShips from "../../lib/generateRandomShips";
+import { useState } from "react";
 import Play from "./components/Play";
+import Prepare from "./components/Prepare";
 
 export default function PlayPage() {
     const [started, setStarted] = useState(false);
-    const [ships, setShips] = useState([]);
-    const [direction, setDirection] = useState("right");
-    const [currentShip, setCurrentShip] = useState(null);
+    const [playerShips, setPlayerShips] = useState([]);
 
-    useEffect(() => {
-        if (!started) {
-            function directionOnScroll(e) {
-                const directions = ["right", "down", "left", "up"];
-
-                let current_direction_index = directions.indexOf(direction);
-
-                if (e.deltaY > 0) {
-                    // if scrolling down, change direction clockwise
-                    if (current_direction_index === 3) current_direction_index = 0;
-                    else current_direction_index++;
-                } else {
-                    // if scrolling up, change direction counter-clockwise
-                    if (current_direction_index === 0) current_direction_index = 3;
-                    else current_direction_index--;
-                }
-
-                setDirection(directions[current_direction_index]);
-            }
-
-            document.addEventListener("wheel", directionOnScroll);
-
-            return () => {
-                document.removeEventListener("wheel", directionOnScroll);
-            };
-        }
-    }, [direction, started]);
-
-    useEffect(() => {
-        if (!started) updateCurrentShip(); // update current ship when direction changes
-    }, [direction, started]);
-
-    function updateCurrentShip(row = currentShip?.cells[0].row, column = currentShip?.cells[0].column) {
-        if (ships.length < 5) {
-            const new_ship = generateShipObject(direction, 5 - ships.length, row, column);
-
-            let invalid = isShipInvalid(new_ship, ships);
-
-            if (invalid) new_ship.invalid = true;
-
-            setCurrentShip(new_ship);
-        }
-    }
-
-    function addCurrentShip() {
-        if (currentShip && !currentShip.invalid) {
-            setShips((ships) => [...ships, currentShip]);
-            setCurrentShip(null);
-        }
-    }
-
-    if (started) return <Play initialPlayerShips={ships} />;
-    else
-        return (
-            <div>
-                <h1>place yo ships</h1>
-                <button onClick={() => setShips(generateRandomShips())}>random placement</button>
-                <button onClick={() => setShips([])}>reset board</button>
-                <Board
-                    onCellHover={updateCurrentShip}
-                    currentShip={currentShip}
-                    onCellClick={addCurrentShip}
-                    ships={ships}
-                    showOccupied={true}
-                />
-                {ships.length >= 5 ? (
-                    <div>
-                        you placed all ships <button onClick={() => setStarted(true)}>Start</button>
-                    </div>
-                ) : (
-                    <>
-                        <div>current direction: {direction}</div>
-                        <div>you have {5 - ships.length} ships left to place</div>
-                    </>
-                )}
-            </div>
-        );
+    if (started) return <Play initialPlayerShips={playerShips} />;
+    else return <Prepare startGame={() => setStarted(true)} ships={playerShips} setShips={setPlayerShips} />;
 }
